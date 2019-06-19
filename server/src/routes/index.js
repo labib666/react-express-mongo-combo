eecacecca25ad49cfde257a79c84d9e8ec4f44e9
Labib666 <ewmahadicd@gmail.com>
2019-06-20 05:48:32 +0600

    3: linted files

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-var router = Router();
+import express from 'express';
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Express'});
 });
 
 export default router;

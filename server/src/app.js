3f582a1fe9baf3c3a7af770a40c05833998d88ff
Labib666 <ewmahadicd@gmail.com>
2019-06-20 23:59:57 +0600

    4: removed view from server

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,8 @@
 import express, {json, urlencoded} from 'express';
-import {join} from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index';
-import usersRouter from './routes/users';
 
 const app = express();
 
@@ -12,9 +10,7 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, '../public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 export default app;
